67b509896600e9dc7e313af1d7d7d080cf318a49
gwhitman2002 <guswhitman@icloud.com>
2023-06-02 13:49:14 -0700

    i got the camera working

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class MoveCamera : MonoBehaviour
+{ 
     // Start is called before the first frame update
     public Transform cameraPosition;
 
